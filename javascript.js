43d954b70d11c95992a1cc781c451eea838f46ce
irennaviolla <irennaviollan@gmail.com>
2023-09-17 23:30:54 +0700

    BUAT DEVITA

---
+++ javascript.js
@@ -0,0 +1,23 @@
+var pass = document.getElementById("psw");
+var blackContainer = document.getElementById("black-container");
+var container = document.getElementById("container");
+
+function klik() {
+    blackContainer.style.display = "flex";
+    container.style.display = "flex";
+}
+
+
+blackContainer.addEventListener('click', () => {
+    blackContainer.style.display = "none";
+    container.style.display = "none";
+})
+
+function checkPassword(){
+    if(pass.value != "310402"){
+        alert("Cie bukan depita")
+    }
+    else {
+        window.location.pathname = "/second.html";
+    }
+}
